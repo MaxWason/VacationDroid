8b07f5857dbf8fde22ca5efbd40f5e2b11372ee1
gr8pefish <xwason@gmail.com>
2015-11-17 09:01:33 +0100

    Added searchbar directly to the VacationListActivity

--- app/src/main/java/com/jkpg/jurgen/nl/vacationdroid/core/vacationList/VacationListActivity.java
+++ app/src/main/java/com/jkpg/jurgen/nl/vacationdroid/core/vacationList/VacationListActivity.java
@@ -1,12 +1,16 @@
 package com.jkpg.jurgen.nl.vacationdroid.core.vacationList;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -34,34 +38,60 @@ public class VacationListActivity extends AppCompatActivity implements Vacations
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
     }
+
+    private void doMySearch(String query){
+        //TODO: api web call here (with current user/friend as other search param)
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_searchable_test, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_menu_test).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        //TODO: handle cases
-        if (id == R.id.action_search) {
-            Intent intent = new Intent(VacationListActivity.this, SearchableActivity.class);
-            startActivity(intent);
-            return false; //true or false?
-        } else if (id == R.id.action_unfriend) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        //TODO: handle cases
+//        if (id == R.id.action_search) {
+//            Intent intent = new Intent(VacationListActivity.this, SearchableActivity.class);
+//            startActivity(intent);
+//            return false; //true or false?
+//        } else if (id == R.id.action_unfriend) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public void onFragmentInteraction(String id) {
