a78c598305758c2bff6af942ea12afdf4427a70d
gr8pefish <xwason@gmail.com>
2015-12-04 17:48:08 +0100

    update vacation data item to be a more complete class

--- app/src/main/java/com/jkpg/jurgen/nl/vacationdroid/Datamodels/Vacation.java
+++ app/src/main/java/com/jkpg/jurgen/nl/vacationdroid/Datamodels/Vacation.java
@@ -1,13 +1,56 @@
 package com.jkpg.jurgen.nl.vacationdroid.datamodels;
 
-/**
- * Created by Jurgen on 11/5/2015.
- */
 public class Vacation {
-    String title;
-    String description;
-    String place;
-    int start;
-    int end;
+    private int id;
+    private String title;
+    private String description;
+    private String place;
+    private int start;
+    private int end;
+    private int userId;
+
+    public Vacation(int id, String title, String description, String place, int start, int end, int userId){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.place = place;
+        this.start = start;
+        this.end = end;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString(){
+        return "[Vacation] id:"+id+"\ttitle:"+title+"\tdescription:"+description+"place:"+place+"\tstart:"+start+"\tend:"+end+"\tuserId:"+userId;
+    }
 
 }
+
